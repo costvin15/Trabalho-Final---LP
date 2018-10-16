a4d11c5753897ed548ae102454436d3d4ce4d68b
Vinicius Costa Castro <costacastrovinicius7@gmail.com>
2018-10-15 22:47:37 -0300

    Inicio do desenvolvimento da interface grafica

--- main.c
+++ main.c
@@ -1,80 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "headers/produtos.h"
+#include <gtk/gtk.h>
 #include "headers/clientes.h"
 
-void adicionando(Produtos *);
-void lendo(Produtos *);
-void buscando(Produtos *);
+static void printHello(GtkWidget *button, GtkWindow *window){
+    GtkWidget *message;
+    message = gtk_message_dialog_new(window, GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, "Hey, there!");
 
-int main(void){
-    Produtos *lista;
-    lista = criar_produtos();
-
-    popular_produtos(lista);
-    
-    adicionando(lista);
-    lendo(lista);
-
-    buscando(lista);
-    salvar_produtos(lista);
-
-    apagar_produtos(lista);
-    return 0;
+    gtk_dialog_run(GTK_DIALOG(message));
 }
 
-void adicionando(Produtos *lista){
-    printf("***** ADICIONAR *****\n");
-    
-    struct produto p;
-    while (1){
-        printf("Nome: ");
-        gets(p.nome);
-        printf("Em estoque: ");
-        scanf("%d", &p.EmEstoque);
-        printf("Preco: ");
-        scanf("%lf", &p.preco);
-
-        adicionar_produto(lista, p);
-
-        char cancontinue;
-        fflush(stdin);
-        printf("Continuar? (S/N): ");
-        scanf("%c", &cancontinue);
-        if (cancontinue == 'N')
-            break;
-    }
-}
-
-void lendo(Produtos *lista){
+static void activate(GtkApplication *app, gpointer user_data){
+    GtkWindow *window;
+    GtkWidget *box, *header, *list, *label, *addbutton;
     int i;
-    printf("Tam: %d\n", lista->tamanho);
-    for (i = 0; i < lista->tamanho; i++){
-        printf("***** PRODUTO *****\n");
-        printf("Nome: %s\n", lista->produtos[i].nome);
-        printf("Em estoque: %d\n", lista->produtos[i].EmEstoque);
-        printf("Preco: %lf\n", lista->produtos[i].preco);
+
+    window = GTK_WINDOW(gtk_application_window_new(app));
+    header = gtk_header_bar_new();
+    addbutton = gtk_button_new_with_label("Adicionar Novo");
+
+    printf("1 -> %p\n", &window);
+    
+    gtk_header_bar_set_title(GTK_HEADER_BAR(header), "Sistema de Loja NerdZ");
+    gtk_header_bar_set_subtitle(GTK_HEADER_BAR(header), "Cadastro de Clientes");
+    gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(header), TRUE);
+    gtk_header_bar_set_custom_title(GTK_HEADER_BAR(header), addbutton);
+
+    gtk_window_set_titlebar(window, header);
+    gtk_window_set_default_size(window, 600, 480);
+    gtk_widget_show_all(GTK_WIDGET(window));
+
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_add(GTK_CONTAINER(window), box);
+
+    Clientes *c = criar_clientes();
+    popular_clientes(c);
+
+    list = gtk_list_box_new();
+    for (i = 0; i < c->tamanho; i++){
+        label = gtk_label_new(c->clientes[i].nome);
+        gtk_list_box_insert(GTK_LIST_BOX(list), label, i);
     }
+
+    g_signal_connect(addbutton, "clicked", G_CALLBACK(printHello), window);
+
+    gtk_container_add(GTK_CONTAINER(box), list);
+    gtk_widget_show_all(GTK_WIDGET(window));
 }
 
-void buscando(Produtos *lista){
-    printf("***** BUSCAR *****\n");
-    printf("Pesquisar: ");
-    
-    int ocorrencias;
-    char termo[50];
-    fflush(stdin);
-    gets(termo);
+int main(int argc, char **argv){
+    GtkApplication *app;
+    int status;
 
-    struct produto **resultados = buscar_produto(lista, termo, &ocorrencias);
+    app = gtk_application_new("com.viniciuscosta.app", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
 
-    printf("Encontramos %d resultados.\n", ocorrencias);
-    int i;
-    for (i = 0; i < ocorrencias; i++){
-        printf("***** PRODUTO *****\n");
-        printf("Nome: %s\n", resultados[i]->nome);
-        printf("Em estoque: %d\n", resultados[i]->EmEstoque);
-        printf("Preco: %lf\n", resultados[i]->preco);    
-    }
+    return status;
 }
